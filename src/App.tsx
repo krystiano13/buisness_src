5e5c80b629cc8751c0915fb571479679baf092cb
krystiano13 <crystianotv@gmail.com>
2023-02-15 21:28:58 +0100

    mobile version of homepage and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,18 @@ import Home from "./pages/Home";
 import { Navbar } from "./molecules/Navbar/Navbar";
 
 const App = () => {
+  const [portrait,setPortrait] = React.useState<boolean>(false);
+  React.useEffect(() => {
+    const media = window.matchMedia("(orientation : portrait)");
+    setPortrait(media.matches);
+    window.matchMedia("(orientation : portrait)").addEventListener('change', (e) => {
+      setPortrait(e.matches);
+    });
+  },[]);
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
+        <Navbar portrait={portrait}/>
         <Routes>
           <Route path="/buisness/" element={<Home />} />
         </Routes>
--- src/molecules/Navbar/Navbar.tsx
+++ src/molecules/Navbar/Navbar.tsx
@@ -1,19 +1,51 @@
 import React from "react";
 import "./Navbar.css";
 
-const Navbar = () => {
+interface NavbarInterface {
+  portrait : boolean
+}
+
+const Navbar:React.FC<NavbarInterface> = ({ portrait }) => {
+  const menu = React.useRef<HTMLElement>(null);
+  const handleMenuShow = () => menu.current?.classList.toggle('shown');
   return (
-    <nav className="Navbar">
-      <p className="Navbar__logo">Business</p>
-      <section className="Navbar__buttons">
-        <button className="Navbar__buttons__button">ABOUT</button>
-        <button className="Navbar__buttons__button">WORK</button>
-        <button className="Navbar__buttons__button">TEAM</button>
-        <button className="Navbar__buttons__button">BLOG</button>
-        <button className="Navbar__buttons__button">CONTACT</button>
-      </section>
-      <button className="Navbar__contactButton">CONTACT US</button>
-    </nav>
+    <>
+      <nav className="Navbar">
+        <p className="Navbar__logo">Business</p>
+
+        {portrait === false && (
+          <>
+            <section className="Navbar__buttons">
+              <button className="Navbar__buttons__button">ABOUT</button>
+              <button className="Navbar__buttons__button">WORK</button>
+              <button className="Navbar__buttons__button">TEAM</button>
+              <button className="Navbar__buttons__button">BLOG</button>
+              <button className="Navbar__buttons__button">CONTACT</button>
+            </section>
+            <button className="Navbar__button">CONTACT US</button>
+          </>
+        )}
+
+        {portrait === true && (
+          <>
+            <div onClick={handleMenuShow} className="Navbar__hamburgerMenu">
+              <section id="left" className="Navbar__hamburgerMenu__stripe" />
+              <section className="Navbar__hamburgerMenu__stripe" />
+              <section id="right" className="Navbar__hamburgerMenu__stripe" />
+            </div>
+          </>
+        )}
+      </nav>
+      {portrait === true && (
+        <section ref={menu} className="ButtonsMenu">
+          <button className="ButtonsMenu__button">ABOUT</button>
+          <button className="ButtonsMenu__button">WORK</button>
+          <button className="ButtonsMenu__button">TEAM</button>
+          <button className="ButtonsMenu__button">BLOG</button>
+          <button className="ButtonsMenu__button">CONTACT</button>
+        </section>
+      )}
+    </>
   );
 };
 
